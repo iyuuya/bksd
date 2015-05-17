61b5eabfd66a8cfe5c755a5ebaed7071b37672d6
iyuuya <i.yuuya@gmail.com>
2015-05-17 23:55:02 +0900

    git subtree push all trees

---
+++ script/gstps.sh
@@ -0,0 +1,6 @@
+git subtree push --prefix=vim vim master
+git subtree push --prefix=zsh zsh master
+git subtree push --prefix=git git master
+git subtree push --prefix=tmux tmux master
+git subtree push --prefix=ruby ruby master
+git subtree push --prefix=brew brew master
