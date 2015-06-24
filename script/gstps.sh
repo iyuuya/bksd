eeb3e646ea46ca30b9db11bf476fe40221ddf3f8
iyuuya <i.yuuya@gmail.com>
2015-06-25 00:12:47 +0900

    align

--- script/gstps.sh
+++ script/gstps.sh
@@ -1,7 +1,7 @@
-git subtree push --prefix=vim vim master
+git subtree push --prefix=vim  vim  master
 git subtree push --prefix=nvim nvim master
-git subtree push --prefix=zsh zsh master
-git subtree push --prefix=git git master
+git subtree push --prefix=zsh  zsh  master
+git subtree push --prefix=git  git  master
 git subtree push --prefix=tmux tmux master
 git subtree push --prefix=ruby ruby master
 git subtree push --prefix=brew brew master
