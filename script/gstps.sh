16248b8b2f9cfc89492fde14bf2990050106590d
iyuuya <i.yuuya@gmail.com>
2015-06-25 00:12:03 +0900

    add nvim to gstps script

--- script/gstps.sh
+++ script/gstps.sh
@@ -1,4 +1,5 @@
 git subtree push --prefix=vim vim master
+git subtree push --prefix=nvim nvim master
 git subtree push --prefix=zsh zsh master
 git subtree push --prefix=git git master
 git subtree push --prefix=tmux tmux master
