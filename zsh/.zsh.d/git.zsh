8ca69f255704698da44b2f60bcae6bc3429969a6
iyuuya <i.yuuya@gmail.com>
2016-02-24 02:29:26 +0900

    add success snap for git+rspec

--- zsh/.zsh.d/git.zsh
+++ zsh/.zsh.d/git.zsh
@@ -6,3 +6,12 @@ function git-grep-replace() {
   fi
   git grep -l $1 | xargs sed -i '' -e "$2"
 }
+
+function success-snap() {
+  bundle exec rspec $target
+  if [ $? -eq 0 ]; then
+    git add .
+    git commit -m "[wip] success snap"
+  fi
+}
+alias ss=success-snap
