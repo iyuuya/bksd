7d5fdd7df6301d08a8baa4e73a431a1390de2887
iyuuya <vim.ito@1pac.jp>
2016-04-28 19:42:42 +0900

    fix gstps script

--- script/gstpsf.sh
+++ script/gstpsf.sh
@@ -1,11 +1,13 @@
 function gstpsf () {
   git push $1 `git subtree split --prefix $1 master`:master --force
 }
-gstpsf brew &
-gstpsf git &
-gstpsf nvim &
-gstpsf ruby &
-gstpsf ssh &
-gstpsf tmux &
-gstpsf zsh &
+gstps brew &
+gstps git &
+gstps mysql &
+gstps node &
+gstps nvim &
+gstps ruby &
+gstps ssh &
+gstps tmux &
+gstps zsh &
 wait
