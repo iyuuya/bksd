4c80205ac826232341e0f515e18a9953ece26bf2
iyuuya <i.yuuya@gmail.com>
2017-10-10 15:07:09 +0900

    add add_remotes.sh script

---
+++ script/add_remotes.sh
@@ -0,0 +1,10 @@
+git remote add brew github.com:iyuuya/brew-config.git
+git remote add git github.com:iyuuya/git-config.git
+git remote add mysql github.com:iyuuya/mysql-config.git
+git remote add node github.com:iyuuya/node-config.git
+git remote add nvim github.com:iyuuya/nvim-config.git
+git remote add ruby github.com:iyuuya/ruby-config.git
+git remote add ssh github.com:iyuuya/ssh-config.git
+git remote add tmux github.com:iyuuya/tmux-config.git
+git remote add vim github.com:iyuuya/vim-config.git
+git remote add zsh github.com:iyuuya/zsh-config.git
