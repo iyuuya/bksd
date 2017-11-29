b254794b9bcb0d946e76b67be46b3165ece39fdb
iyuuya <i.yuuya@gmail.com>
2017-11-29 19:17:37 +0900

    anyenvのタスク定義をメソッドで分割

--- tasks/anyenv_setup_task.rb
+++ tasks/anyenv_setup_task.rb
@@ -3,32 +3,23 @@ require 'pathname'
 
 class AnyenvSetupTask < Rake::TaskLib
   def initialize(name)
-    anyenv_tasks = ['anyenv:install', 'anyenv:install_plugins']
+    @name = name
+    @anyenv_tasks = []
     @anyenv_home = Pathname.new(File.expand_path('~/.anyenv'))
 
-    namespace :anyenv do
-      desc 'install anyenv'
-      task :install do
-        if Dir.exist?(@anyenv_home)
-          puts 'anyenv exist'
-        else
-          system "git clone https://github.com/riywo/anyenv #{@anyenv_home}"
-        end
-      end
+    define
+  end
 
-      desc 'install anyenv plugins'
-      task :install_plugins do
-        path = @anyenv_home.join('plugins/anyenv-update')
-        if Dir.exist?(path)
-          puts 'anyenv-update exist'
-        else
-          system "git clone https://github.com/znz/anyenv-update #{path}"
-        end
-      end
+  private
+
+  def define
+    namespace :anyenv do
+      define_install_anyenv
+      define_install_anyenv_plugins
 
       {
         rbenv: [
-          { name: 'rbenv-default-gems', repository: 'https://github.com/rbenv/rbenv-default-gems' },
+          { name: 'rbenv-default-gems', repository: 'https://github.com/rbenv/rbenv-default-gems' }
         ],
         pyenv: [
           { name: 'pyenv-default-packages', repository: 'https://github.com/jawshooah/pyenv-default-packages' },
@@ -36,31 +27,50 @@ class AnyenvSetupTask < Rake::TaskLib
           { name: 'pyenv-update', repository: 'https://github.com/yyuu/pyenv-update' },
           { name: 'pyenv-which-ext', repository: 'https://github.com/yyuu/pyenv-which-ext' },
           { name: 'pyenv-pip-rehash', repository: 'https://github.com/yyuu/pyenv-pip-rehash' },
-          { name: 'pyenv-virtualenv', repository: 'https://github.com/yyuu/pyenv-virtualenv' },
+          { name: 'pyenv-virtualenv', repository: 'https://github.com/yyuu/pyenv-virtualenv' }
         ],
         ndenv: [
-          { name: 'ndenv-default-npms', repository: 'https://github.com/kaave/ndenv-default-npms' },
+          { name: 'ndenv-default-npms', repository: 'https://github.com/kaave/ndenv-default-npms' }
         ],
         phpenv: [
-          { name: 'phpenv-composer', repository: 'https://github.com/ngyuki/phpenv-composer' },
+          { name: 'phpenv-composer', repository: 'https://github.com/ngyuki/phpenv-composer' }
         ],
-        goenv: [
-        ],
+        goenv: []
       }.each do |xxenv, plugins|
-        task_name = define_install_xxenv(xxenv)
-        anyenv_tasks << "anyenv:#{task_name}"
-
+        define_install_xxenv(xxenv)
         plugins.each do |plugin|
-          task_name = define_install_xxenv_plugin(xxenv, plugin[:name], plugin[:repository])
-          anyenv_tasks << "anyenv:#{task_name}"
+          define_install_xxenv_plugin(xxenv, plugin[:name], plugin[:repository])
         end
       end
     end
 
-    task name => anyenv_tasks
+    task @name => @anyenv_tasks
   end
 
-  private
+  def define_install_anyenv
+    desc 'install anyenv'
+    task :install do
+      if Dir.exist?(@anyenv_home)
+        puts 'anyenv exist'
+      else
+        system "git clone https://github.com/riywo/anyenv #{@anyenv_home}"
+      end
+    end
+    @anyenv_tasks << 'anyenv:install'
+  end
+
+  def define_install_anyenv_plugins
+    desc 'install anyenv plugins'
+    task :install_plugins do
+      path = @anyenv_home.join('plugins/anyenv-update')
+      if Dir.exist?(path)
+        puts 'anyenv-update exist'
+      else
+        system "git clone https://github.com/znz/anyenv-update #{path}"
+      end
+    end
+    @anyenv_tasks << 'anyenv:install_plugins'
+  end
 
   def define_install_xxenv(xxenv)
     task_name = "install_#{xxenv}"
@@ -73,7 +83,7 @@ class AnyenvSetupTask < Rake::TaskLib
         system "PATH=~/.anyenv/bin:$PATH eval \"$(anyenv init - --no-rehash)\"; anyenv install #{xxenv}"
       end
     end
-    task_name
+    @anyenv_tasks << "anyenv:#{task_name}"
   end
 
   def define_install_xxenv_plugin(xxenv, name, repository)
@@ -87,6 +97,6 @@ class AnyenvSetupTask < Rake::TaskLib
         system "git clone #{repository} #{path}"
       end
     end
-    task_name
+    @anyenv_tasks << "anyenv:#{task_name}"
   end
 end
