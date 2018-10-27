9a307112f9ca64cab288bceff51aa7ed16543228
iyuuya <i.yuuya@gmail.com>
2018-10-27 16:21:24 +0900

    remove squashed build

--- dockerfiles/nvim/build.sh
+++ dockerfiles/nvim/build.sh
@@ -15,21 +15,5 @@ build() {
   docker build -t "${REPO}:${tag}" ${opt} .
 }
 
-build_squash() {
-  if [ $# = 0 ]; then
-    tag=latest
-    opt=
-  elif [ $# = 1 ]; then
-    tag="$1"
-    opt=" --build-arg v$1"
-  else
-    exit 1
-  fi
-  docker build --squash=true -t "${REPO}:${tag}-squash" ${opt} .
-}
-
 build
 build 0.3.1
-
-build_squash
-build_squash 0.3.1
--- dockerfiles/tmux/build.sh
+++ dockerfiles/tmux/build.sh
@@ -15,21 +15,5 @@ build() {
   docker build -t "${REPO}:${tag}" ${opt} .
 }
 
-build_squash() {
-  if [ $# = 0 ]; then
-    tag=latest
-    opt=
-  elif [ $# = 1 ]; then
-    tag="$1"
-    opt=" --build-arg $1"
-  else
-    exit 1
-  fi
-  docker build --squash=true -t "${REPO}:${tag}-squash" ${opt} .
-}
-
 build
 build 2.8
-
-build_squash
-build_squash 2.8
